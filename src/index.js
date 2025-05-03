c7f013ecb9ec173e0aaac36cea82536b3bace7a4
Kartikeya Kesarwani <kartikeyakesarwaniv@gmail.com>
2025-05-03 16:14:24 +0530

    Initial Setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>
+      <h1>Hello world</h1>
+      <h2>Welcome to my React app</h2>
+    </div>
+  </React.StrictMode>
+);
